13de055fa95422b36a3483b2ceb02cd373a88a89
Zuyu3 <leiyuwang33@gmail.com>
2026-06-08 09:00:50 +0000

    Deploying to gh-pages from @ Zuyu3/Zuyu3-cn.github.io@93949916e994fad0949d587f8ca9221944c5d7c4 🚀

--- assets/js/cookie-consent-setup.js
+++ assets/js/cookie-consent-setup.js
@@ -96,7 +96,7 @@ function initializeCookieConsent() {
               },
               {
                 title: 'More information',
-                description: 'For any queries in relation to our policy on cookies and your choices, please <a class="cc-link" href="https://Zuyu3.github.io//#contact">contact us</a>.'
+                description: 'For any queries in relation to our policy on cookies and your choices, please <a class="cc-link" href="https://zuyu3.github.io/Zuyu3-cn.github.io//#contact">contact us</a>.'
               }
             ]
           }
